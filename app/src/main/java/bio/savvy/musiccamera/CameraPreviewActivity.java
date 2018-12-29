3807f6cdbcac93d36413392b165b1663208be95e
djsavvy <github.com/djsavvy>
2018-12-29 01:21:53 -0600

    Get initial preview working

--- app/src/main/java/bio/savvy/musiccamera/CameraPreviewActivity.java
+++ app/src/main/java/bio/savvy/musiccamera/CameraPreviewActivity.java
@@ -12,13 +12,14 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Collections;
+
+import static android.support.constraint.ConstraintLayout.*;
 
 
 public class CameraPreviewActivity extends AppCompatActivity {
@@ -32,6 +33,55 @@ public class CameraPreviewActivity extends AppCompatActivity {
     // Camera management
     private CameraManager cameraManager_;
     private CameraDevice cameraDevice_;
+
+    private final SurfaceHolder.Callback surfaceHolderCallback_ = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                        /* TODO: This is never getting called -- to fix this, create my own CameraPreview class that extends SurfaceView and implements SurfaceHolder.Callback
+                        https://stackoverflow.com/questions/5912053/surfacecreated-is-not-called
+                         */
+                Log.e(LOG_TAG, "Surface Created");
+//                        previewHolder_.setSizeFromLayout();
+                cameraDevice_.createCaptureSession(Collections.singletonList(previewHolder_.getSurface()), new CameraCaptureSession.StateCallback() {
+                    @Override
+                    public void onConfigured(@NonNull CameraCaptureSession session) {
+                        try {
+                            CaptureRequest.Builder requestBuilder = cameraDevice_.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                            requestBuilder.addTarget(previewHolder_.getSurface());
+                            session.setRepeatingRequest(requestBuilder.build(), new CameraCaptureSession.CaptureCallback() {
+                                @Override
+                                public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+                                }
+                            }, null);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                    }
+                }, null);
+            }
+            catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+    };
+
     private final CameraDevice.StateCallback deviceStateCallback_ = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
@@ -39,53 +89,14 @@ public class CameraPreviewActivity extends AppCompatActivity {
             Log.e(LOG_TAG, "cameraDevice_ set to " + cameraDevice_.toString());
 
             // Set up preview
+//            previewSurfaceView_ = new SurfaceView(CameraPreviewActivity.this);
             previewSurfaceView_ = findViewById(R.id.previewSurfaceView);
             previewHolder_ = previewSurfaceView_.getHolder();
-            previewHolder_.addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(SurfaceHolder holder) {
-                    try {
-                        /* TODO: This is never getting called -- to fix this, create my own CameraPreview class that extends SurfaceView and implements SurfaceHolder.Callback
-                        https://stackoverflow.com/questions/5912053/surfacecreated-is-not-called
-                         */
-                        Log.e(LOG_TAG, "Surface Created");
-//                        previewHolder_.setSizeFromLayout();
-                        cameraDevice_.createCaptureSession(Collections.singletonList(previewHolder_.getSurface()), new CameraCaptureSession.StateCallback() {
-                            @Override
-                            public void onConfigured(@NonNull CameraCaptureSession session) {
-                                try {
-                                    CaptureRequest.Builder requestBuilder = cameraDevice_.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                                    session.setRepeatingRequest(requestBuilder.build(), new CameraCaptureSession.CaptureCallback() {
-                                        @Override
-                                        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
-                                            super.onCaptureStarted(session, request, timestamp, frameNumber);
-                                        }
-                                    }, null);
-                                } catch (CameraAccessException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            @Override
-                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
-                            }
-                        }, null);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                    surfaceCreated(holder);
-                }
-
-                @Override
-                public void surfaceDestroyed(SurfaceHolder holder) {
-
-                }
-            });
+//            previewHolder_.addCallback(surfaceHolderCallback_);
+//            previewSurfaceView_.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT));
+//            ((ConstraintLayout) findViewById(R.id.previewLayout)).addView(previewSurfaceView_);
+            surfaceHolderCallback_.surfaceCreated(previewHolder_);
+            Log.e(LOG_TAG, "View added");
         }
 
         @Override
